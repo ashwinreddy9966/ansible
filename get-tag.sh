fea2052f4262cf60ae1cb7ee8f2e2625a9d0d594
manu <manu@local.com>
2022-05-03 11:39:43 +0530

    Auto Push Commit

--- get-tag.sh
+++ get-tag.sh
@@ -4,6 +4,7 @@ C1=$(git tag -l | awk -F . '{print $1}' |sort -n  |uniq  | tail -1)
 C2=$(git tag -l | grep ^${C1} | awk -F . '{print $2}' | sort -n | uniq |tail -1)
 
 C3=$(git tag -l | grep ^${C1}\.${C2}  | awk -F . '{print $3}' |sort -n | uniq | tail -1)
+C3=$((C3+1))
 
 version="$C1.$C2.$C3"
 echo $version
