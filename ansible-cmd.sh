956dddd33e48066cffdab3af57640ce820ed921e
manu <manu@local.com>
2022-06-01 10:52:35 +0530

    Auto Push Commit

--- ansible-cmd.sh
+++ ansible-cmd.sh
@@ -1,3 +1,5 @@
 ansible-pull -U https://github.com/ashwinreddy9966/ansible roboshop-pull.yml -e ENV=dev -e APP_VERSION=1.0.5 -e COMPONENT=cart
 
 ansible-pull -U https://github.com/ashwinreddy9966/ansible roboshop-pull.yml -e role_name=elk -e COMPONENT=cart
+
+ansible-pull -U https://github.com/ashwinreddy9966/ansible roboshop-pull.yml -e COMPONENT=elk
